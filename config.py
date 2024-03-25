53a78da36881f37ce39b23c5f7de5d12dbb988a2
nationalindianagencyproducedbynasainus <133997312+nationalindianagencyproducedbynasainus@users.noreply.github.com>
2024-03-25 19:45:08 +0530

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config:
-    API_ID=24509589
-    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    TOKEN= "6841680264:AAFgVWVmPBdXZyyMQkTMO8pmXaf52o95wqw"
+    API_ID=25105744
+    API_HASH="0ca4154111e7b0f99e9929710faa3f25"
+    TOKEN="6841680264:AAFgVWVmPBdXZyyMQkTMO8pmXaf52o95wqw"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=6796545941
