e39ef8122ed2acec32d590f9fa483b885d837986
nationalindianagencyproducedbynasainus <133997312+nationalindianagencyproducedbynasainus@users.noreply.github.com>
2024-03-25 19:48:08 +0530

    config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     TOKEN="6841680264:AAFgVWVmPBdXZyyMQkTMO8pmXaf52o95wqw"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=6796545941
+    BOT_ID=6841680264
     BOT_USERNAME="useremovebot"
     BOT_NAME="useremovebot"
