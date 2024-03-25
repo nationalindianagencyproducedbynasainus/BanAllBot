22b1d4e82aa552ecfd65dcb3e16dc1ce353a2c00
nationalindianagencyproducedbynasainus <133997312+nationalindianagencyproducedbynasainus@users.noreply.github.com>
2024-03-25 20:26:53 +0530

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_ID=25105744
     API_HASH="0ca4154111e7b0f99e9929710faa3f25"
     TOKEN="6841680264:AAFgVWVmPBdXZyyMQkTMO8pmXaf52o95wqw"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6573541531").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=6841680264
     BOT_USERNAME="useremovebot"
