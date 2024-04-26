ee0d71c529d0f6d158c152ae3f62458381b81960
nationalindianagencyproducedbynasainus <133997312+nationalindianagencyproducedbynasainus@users.noreply.github.com>
2024-04-26 17:29:24 +0530

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     API_ID=25105744
     API_HASH="0ca4154111e7b0f99e9929710faa3f25"
-    TOKEN="6841680264:AAFgVWVmPBdXZyyMQkTMO8pmXaf52o95wqw"
+    TOKEN="6841680264:AAE0IXoBMmqSpAH_9hOTlns1bzPfhaBFuYY"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6573541531").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=6841680264
