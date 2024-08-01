d5fc5c79f38df3c4f255508e5d1132409c5dfdb4
nationalindianagencyproducedbynasainus <133997312+nationalindianagencyproducedbynasainus@users.noreply.github.com>
2024-08-01 17:02:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 class Config:
     API_ID=25105744
     API_HASH="0ca4154111e7b0f99e9929710faa3f25"
-    TOKEN="6841680264:AAE0IXoBMmqSpAH_9hOTlns1bzPfhaBFuYY"
+    TOKEN="6984962590:AAFmtV1wtAZLIT03h-F0575t_rtpGnpn0ac"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6573541531").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=6841680264
-    BOT_USERNAME="useremovebot"
-    BOT_NAME="useremovebot"
+    BOT_ID=6984962590
+    BOT_USERNAME="WizardInfoBot"
+    BOT_NAME="WizardInfoBot"
